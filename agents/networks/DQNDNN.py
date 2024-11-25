6a76ec6ced04cf49d66216207ecb928da22da889
DDldl2665 <104444386+DDldl2665@users.noreply.github.com>
2024-11-25 21:29:26 +0800

    pipe count

--- agents/networks/DQNDNN.py
+++ agents/networks/DQNDNN.py
@@ -1,21 +1,21 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class DQNDNN(nn.Module):
     def __init__(self, 
-                 input_dim,
+                 input_dim: int,
                  n_actions: int) -> None:
         super().__init__()
 
-        self.fc1 = nn.Linear(input_dim, 64)
-        self.fc2 = nn.Linear(64, 64)
+        # 全連接層
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 32)
-        self.fc4 = nn.Linear(32, 32)
-        self.fc5 = nn.Linear(32, n_actions)
+        self.fc4 = nn.Linear(32, n_actions)
 
     def forward(self, state):
-        hid = torch.sigmoid(self.fc1(state))
-        hid = torch.sigmoid(self.fc2(hid))
-        hid = torch.sigmoid(self.fc3(hid))
-        hid = torch.sigmoid(self.fc4(hid))
-        return self.fc5(hid)
+        x = F.relu(self.fc1(state))  # 使用 ReLU 激活函數
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        return self.fc4(x)  # 輸出 Q 值向量
